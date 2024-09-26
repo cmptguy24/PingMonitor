a3c7ae45f3eb06724c05d6dbb291df33ec342a96
cmptguy24 <cmptguy@gmail.com>
2024-09-26 16:30:55 -0300

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PingMonitor")]
-[assembly: AssemblyCopyright("Copyright © IGT")]
+[assembly: AssemblyCopyright("Copyright © Company")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
